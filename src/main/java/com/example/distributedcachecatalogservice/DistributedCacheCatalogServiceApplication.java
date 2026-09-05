1b7fe94ebc37169db02e5dce254bc320c80f65c0
Jonathan Mascarenhas <mascarenhasjonathan@gmail.com>
2026-09-05 09:45:01 -0700

    Initial commit for distributed cache catalog service

---
+++ src/main/java/com/example/distributedcachecatalogservice/DistributedCacheCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.distributedcachecatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedCacheCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedCacheCatalogServiceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/distributedcachecatalogservice/model/Product.java
@@ -0,0 +1,4 @@
+package com.example.distributedcachecatalogservice.model;
+
+public class Product {
+}
